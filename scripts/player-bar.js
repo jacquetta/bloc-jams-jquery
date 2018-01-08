07b0a6f17319b6f93e095ef061013c8df22afa58
jacquetta <jacquettavb@hotmail.com>
2018-01-07 21:51:07 -0600

    assignment slider complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -33,9 +33,11 @@
   setInterval( () => {
     if (player.playState !== 'playing') { return; }
     const currentTime = player.getTime();
+    const totalTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
     $('#time-control .current-time').text( currentTime );
+    $('#time-control .total-time').text( totalTime);
     $('#time-control input').val(percent);
   }, 1000);
 }
